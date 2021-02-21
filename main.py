ab2d49c041a3b665c999ca888ce47e03c0cc331a
sol-studio <cloud8862@gmail.com>
2021-02-21 14:12:56 +0900

    init

---
+++ main.py
@@ -0,0 +1,286 @@
+# 커스텀 모듈
+from sol import *
+
+# Create Flask App
+app = Flask(__name__)
+ips={}
+NoLoginPages = [
+    "/?",
+    "/signup?",
+    "/login?"
+]
+
+IgnoreConnect = [
+    "/static/",
+    "/plugin/",
+    "/config",
+    "/favicon.ico?",
+    "/manage"
+]
+
+@app.before_request
+def all_connect():
+    ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    for connect in IgnoreConnect:
+        if connect in request.full_path:return
+
+    if "userid" not in session.keys():
+        session["userid"] = None
+
+
+    ips[ip] = {"last": time.time(), "url": request.full_path , "id": session["userid"]}
+    print(ip, request.full_path)
+    for page in NoLoginPages:
+        if page == request.full_path:return
+
+    if not isLogined(session):
+        return redirect("/login")
+
+
+
+
+def isLogined(session):
+    if "userid" in session.keys():
+        return session['userid']
+    else:
+        return False
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", logined=isLogined(session))
+
+
+# /board
+@app.route("/board/")
+@app.route("/board/list")
+def index_of_board():
+    return redirect("/board/list/1")
+
+
+# board/list/
+@app.route("/board/list/<index_num>")
+def pages_(index_num):
+    if not isLogined(session):
+        return redirect("/login")
+    client = MongoClient("mongodb://localhost:27017/")
+    posts = client.sol.posts
+    i = int(index_num) * 20 - 20
+    return_posts = {}
+
+
+    for post in posts.find({
+            'url': {
+                '$gt': posts.estimated_document_count() - (int(index_num) * 20 + 1),
+                '$lt' : posts.estimated_document_count() - (int(index_num) * 20 - 21)
+                }}).sort("url", -1):
+        return_posts[post['url']] = post['title'], post['url'], post['time'], post['author']
+        i += 1
+
+    client.close()
+    return render_template("board/index.html", posts=return_posts, length=list(return_posts.keys()), page=int(index_num))
+
+
+# 글 쓰기
+@app.route("/board/new", methods=['POST', 'GET'])
+def new():
+    if request.method == "GET":
+        return render_template("board/new.html")
+    content = str(request.form.get('content'))
+    if not content:
+        flash("내용을 입력해주세요")
+        return render_template("board/new.html")
+    if not request.form.get('title'):
+        flash("제목을 입력해주세요")
+        return render_template("board/new.html")
+
+
+    ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    client = MongoClient("mongodb://localhost:27017/")
+    posts = client.sol.posts
+    content = str(request.form.get('content'))
+    if "<br>" not in content:
+        content = content.replace("\n", "<br>")
+
+
+    x = posts.insert_one(
+        {
+            "title": request.form.get('title'),
+            "author": session['userid'],
+            "content": content,
+            "url": posts.estimated_document_count() + 1,
+            "time": time.strftime('%c', time.localtime(time.time())),
+            "ip": ip
+        }
+    )
+    client.close()
+    return redirect("/board/list")
+
+
+# 글 보기 (조회)
+@app.route("/board/post/<id_>")
+def post(id_):
+    ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    if not isLogined(session):
+        return redirect("/login")
+
+    client = MongoClient("mongodb://localhost:27017/")
+    posts = client.sol.posts
+    data = posts.find({"url": int(id_)})
+    client.close()
+    try:
+        return render_template("board/post.html", post=data[0], page=data[0]["url"] // 20 + 1)
+    except:
+        return redirect("/err/404")
+
+
+# 글 삭제
+@app.route("/board/post/<id_>/delete")
+def delete_post(id_):
+    client = MongoClient("mongodb://localhost:27017/")
+    posts = client.sol.posts
+    data = posts.find({"url": int(id_)})
+    client.close()
+    if session['userid'] == data[0]['author']:
+        posts.delete_one({"url": int(id_)})
+        flash("삭제됐습니다")
+        return redirect("/board/list")
+    else:
+        flash("권한이 없습니다")
+        return redirect("/board/post/" + id_)
+
+
+# 로그인
+@app.route("/login", methods=['POST','GET'])
+def login():
+    form = LoginForm()
+
+    if request.method == "GET":
+        return render_template("login.html", form=form)
+
+    elif request.method == "POST":
+        # db 연결
+        client = MongoClient("mongodb://localhost:27017/")
+        posts = client.sol.users
+        # form
+        id = form.data.get('id')
+        pw = form.data.get('pw')
+        data = posts.find({"id": id})
+
+        # db 연결 종료
+        client.close()
+
+        # 정보 검색
+        login_data = {}
+        i = 0
+        for d in data:
+            login_data[str(i)] = d
+
+
+        # 아이디가 존재하지 않음
+        if len(login_data) != 1:
+            return redirect("/login")
+
+        # 비밀번호가 안맞음
+        if login_data["0"]["pw"] != pw:
+            return redirect("/login")
+
+        # 통과
+        else:
+            session['userid'] = id
+            return redirect("/")
+
+
+# 회원가입
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    if isLogined(session):
+        flash("이미 로그인되어있습니다.")
+        return redirect("/")
+
+    form = RegisterForm()
+    if request.method == "GET":
+        return render_template("signup.html", form=form)
+
+    elif request.method == "POST":
+        client = MongoClient("mongodb://localhost:27017/")
+        database = client["sol"]
+        users = database["users"]
+        data = users.find({"id": form.data.get('id')})
+        login_data = {}
+        i = 0
+        for d in data:
+            login_data[str(i)] = d
+
+
+        # 아이디가 존재하지 않음
+        if len(login_data) != 1:
+            if form.data.get('pw') != form.data.get('re_password'):
+                flash('비밀번호가 일치하지 않습니다')
+                return render_template('signup.html', form=form)
+
+
+            if len(form.data.get('pw')) < 10:
+                flash("비밀번호는 10글자 이상으로 해주세요")
+                return render_template("signup.html", form=form)
+            x = users.insert_one(
+                {
+                    "id": form.data.get('id'),
+                    "pw": form.data.get('pw'),
+                    "ip": ip,
+                    "name": form.data.get('name')
+                }
+            )
+            client.close()
+            return redirect("/login")
+        else:
+            flash("해당 아이디는 이미 사용중입니다.")
+            return render_template("signup.html", form=form)
+
+
+# 로그아웃
+@app.route("/logout")
+def logout():
+    ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    if not isLogined(session):
+        return redirect("/login")
+    if isLogined:
+        del session['userid']
+        return redirect("/")
+    else:
+        return redirect("/login")
+
+# 관리자 페이지
+@app.route("/manage")
+def manage():
+    if session['userid'] == "관리자" or session['userid'] == "admin":
+        if request.args.get("cmd"):
+            cmd = request.args.get("cmd").split()
+            if cmd[0] == "del":
+                del ips[cmd[1]]
+            return redirect("/manage")
+
+        return_dict = manage_helper(ips)
+        return_str = ""
+        for key in return_dict.keys():
+            return_str += key + "\n&nbsp;&nbsp;ID    : " + str(return_dict[key]["id"]) + "\n&nbsp;&nbsp;URL  : " + return_dict[key]["url"] + "\n&nbsp;&nbsp;TIME : " + return_dict[key]["last"] + "\n\n"
+        return render_template("manage.html", ips=return_str.replace("\n", "<br>").replace(" ", "&nbsp;"))
+    else:
+        return redirect("/login")
+
+# 404
+@app.route("/err/404")
+@app.errorhandler(404)
+def _page_not_found(err=404):
+    return "존재하지 않는 페이지입니다. <a href='/'>홈으로 가기</a>"
+
+
+
+if __name__ == "__main__":
+    Log.log("server restarted")
+
+    app.config['SECRET_KEY'] =  open("secretkey.txt", "r").read()
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    Log.log("server closed")
---
+++ sol.py
@@ -0,0 +1,117 @@
+# IMPORT
+import logging
+import time
+import datetime
+import pprint
+from pytz import timezone
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms import PasswordField
+from wtforms.validators import DataRequired
+from wtforms.validators import EqualTo
+from flask import Flask
+from flask import render_template
+from flask import redirect
+from flask import request
+from flask import make_response
+from flask import session
+from flask import flash
+from pymongo import MongoClient, ASCENDING
+from pytz import timezone
+
+
+
+# LOGIN FORMS
+class RegisterForm(FlaskForm):
+    id = StringField('id', validators=[DataRequired()])
+    name = StringField('name', validators=[DataRequired()])
+    pw = PasswordField('pw', validators=[DataRequired(), EqualTo('re_password')])
+    re_password = PasswordField('re_password', validators=[DataRequired(), EqualTo('pw')])
+
+
+class LoginForm(FlaskForm):
+    id = StringField('id', validators=[DataRequired()])
+    pw = PasswordField('pw', validators=[DataRequired()])
+
+
+class Log():
+    def __init__(self):
+        # LOG SETTING
+        dt = datetime.datetime.now(timezone("Asia/Seoul"))
+        log_date = dt.strftime("%Y%m%d")
+        logging.basicConfig(filename = "logs/" + log_date + "log.log", level = logging.DEBUG)
+
+    # LOG FUCNTIONS
+    def get_log_date(self):
+        dt = datetime.datetime.now(timezone("Asia/Seoul"))
+        log_date = dt.strftime("%Y%m%d_%H:%M:%S")
+        return log_date
+
+    def log(self, message, request=0):
+        log_date = self.get_log_date()
+        log_message = "{0}/{1}/{2}".format(log_date, str(request), message)
+        print(color(log_message, Colors.CYAN))
+        logging.info(log_message)
+
+    def error_log(self, error_message, request=0, error_code=0):
+        log_date = self.get_log_date()
+        log_message = "{0}/{1}/{2}/{3}".format(log_date, str(request), error_code, error_message)
+        print(color(log_message, Colors.MAGENTA))
+        logging.info(log_message)
+
+
+# COLOR
+class Colors:
+    RESET = "\033[0m"
+    RED = "\033[31m"
+    BLACK = "\033[30m"
+    GREEN = "\033[32m"
+    HIDE = "\033[8m"
+    UNDERLINE = "\033[4m"
+    BOLD = "\033[1m"
+    YELLO = "\033[33m"
+    BLUE = "\033[34m"
+    MAGENTA = "\033[35m"
+    CYAN = "\033[36m"
+    WHITE = "\033[37m"
+    BG_BLACK = "\033[40m"
+    BG_GREEN = "\033[42m"
+    BG_YELLOW = "\033[43m"
+    BG_BLUE = "\033[44m"
+    BG_MAGENTA = "\033[45m"
+    BG_CYAN = "\033[46m"
+    BG_WHITE = "\033[47m"
+
+
+def color(string, start_color=Colors.RESET):
+    return start_color + string + Colors.RESET
+
+
+def time_passed(last_time):
+    sec, min, hour, day   = time.time() - last_time, 0, 0, 0
+
+    if sec > 60:
+        min = sec // 60
+        sec -= min * 60
+        if min > 60:
+            hour = min // 60
+            min -= hour * 60
+            if hour > 24:
+                day = hour // 24
+                hour -= day * 24
+
+    return "%d일 %d시간 %d분 %f초" % (day, hour, min, sec)
+
+
+def manage_helper(data):
+    return_dict = {}
+    for key in data.keys():
+        return_dict[key] = {
+            "last": time_passed(data[key]["last"]),
+            "url": data[key]["url"],
+            "id": data[key]["id"]
+        }
+    return return_dict
+
+
+Log = Log()
