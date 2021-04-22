543c8454ac6c1d0129bcf51065ee1adaf93f44c7
sol-studio <cloud8862@gmail.com>
2021-04-22 09:06:38 +0900

    comment test

--- main.py
+++ main.py
@@ -922,6 +922,12 @@ def chat_room(room):
     return render_template("err/chat-no-room.html")
 
 
+def drive_path_check(session, full_path_, path_):
+    if "게시판\\drive\\" + session["userid"] not in os.path.abspath("drive/%s/%s/%s" % (session["userid"], full_path_, str(path_))):
+        return True
+    return False
+
+
 # drive
 @app.route("/drive", methods=["GET", "POST"])
 def drive():
@@ -929,9 +935,6 @@ def drive():
 
     if not full_path:
         full_path = ""
-
-    if "게시판\\drive\\" + session["userid"] not in os.path.abspath("drive/%s/%s" % (session["userid"], full_path)):
-        return "누구인가 누가 해킹을 하려고 하는가!!!"
 
     if request.method == "POST":
         files = request.files.getlist("file[]")
@@ -947,9 +950,24 @@ def drive():
         return redirect("/drive?path=" + full_path)
 
     else:
+
+        # path check
+        if drive_path_check(session, full_path, "")\
+            or drive_path_check(session, full_path, request.args.get("file"))\
+            or drive_path_check(session, full_path, request.args.get("mkdir"))\
+            or drive_path_check(session, full_path, request.args.get("rmdir"))\
+            or drive_path_check(session, full_path, request.args.get("del")):
+            return "누구인가 누가 해킹을 하려고 하는가!!!"
+
+
+
+
         # 그 사람 폴더 없으면 생성
         if not os.path.isdir("drive/" + session["userid"]):
             os.mkdir("drive/" + session["userid"])
+
+
+
 
         if request.args.get("mkdir"):
             try:
@@ -960,32 +978,38 @@ def drive():
                 flash("같은 이름의 폴더가 이미 있거나 폴더 이름으로 사용할 수 없는 기호가 포함돼있습니다.")
                 return redirect("/drive?path=" + full_path)
 
+
+
         if request.args.get("rmdir"):
             shutil.rmtree("drive/%s/%s/%s" % (session["userid"], full_path, request.args.get("rmdir")))
             return redirect("/drive?path=" + full_path)
+
+
 
         if request.args.get("del"):
             try:
                 os.remove("drive/%s/%s/%s" % (session["userid"], full_path, request.args.get("del")))
                 return redirect("/drive?path=" + full_path)
-
             except:
                 flash("해당 파일이 없거나 오류가 발생했습니다.")
                 return redirect("/drive?path=" + full_path)
+
+
 
         if request.args.get("overflow"):
             flash("사용 가능한 용량을 초과했습니다.")
             return ""
 
+
+
         if request.args.get("file"):
-            if "게시판\\drive\\" + session["userid"] not in os.path.abspath(
-                    "drive/%s/%s" % (session["userid"], request.args.get("file"))):
-                return "이 바부야 내가 이걸 안막았겠냐"
             try:
                 return send_file("drive/" + session["userid"] + "/" + request.args.get("file"), as_attachment=True)
 
             except:
                 return ""
+
+
 
         # 상위폴더 경로
         if "/" not in full_path:
@@ -997,7 +1021,6 @@ def drive():
         tmp = os.listdir("drive/%s/%s" % (session["userid"], full_path))
         dirs = {}
         files = {}
-        exten = {}
 
         for dir_ in tmp:
             if os.path.isdir("drive/%s/%s" % (session["userid"], full_path) + "/" + dir_):
@@ -1025,8 +1048,7 @@ def drive():
                                 full_path_l=full_path_l,
                                 upper=upper,
                                 paths=paths[1:],
-                                paths_len=len(paths),
-                                extensions=exten
+                                paths_len=len(paths)
                                 )
 
 
